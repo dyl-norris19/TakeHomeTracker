f7ab146a4918b22560ea54d05ce85e1593722296
Dylan Norris <dylannorris919@gmail.com>
2026-08-06 22:53:58 +0000

    feat: add docker and hosting on local network for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    watch: {
-      usePolling: true,
-    }
-  },
-  resolve: {
-    alias: {
-      $lib: path.resolve("./src/lib"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [svelte()],
+    server: {
+      host: '0.0.0.0',
+      allowedHosts: [env.VITE_ALLOWED_HOSTS],
+      watch: {
+        usePolling: true,
+      }
     },
-  },
+    resolve: {
+      alias: {
+        $lib: path.resolve("./src/lib"),
+      },
+    },
+  }
 })
