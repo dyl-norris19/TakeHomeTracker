aa5abaa739b279ef0479bfc13bf18e64daeda247
Dylan Norris <dylannorris919@gmail.com>
2026-09-13 18:40:12 -0500

    fix: remove the ability for the dialog box to close when interacting with stuff outside of it

--- src/lib/components/ui/dialog/dialog-content.svelte
+++ src/lib/components/ui/dialog/dialog-content.svelte
@@ -26,6 +26,7 @@
 			"data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] bg-background fixed left-[50%] top-[50%] z-50 flex max-h-[85vh] w-full max-w-lg translate-x-[-50%] translate-y-[-50%] flex-col overflow-y-auto gap-4 border px-6 shadow-lg duration-200 sm:rounded-lg",
 			className
 		)}
+		interactOutsideBehavior="ignore"
 		{...restProps}
 	>
 		{@render children?.()}
